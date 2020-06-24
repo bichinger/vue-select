29a2eff248b71cf9e0949e6bdae0445a648283b1
Swen-Thorsten Mock <s.mock@bichinger.de>
2020-06-24 12:57:11 +0200

    Switched to custom object compare method to check equality only.

--- src/components/Select.vue
+++ src/components/Select.vue
@@ -1033,16 +1033,16 @@
        * @returns {boolean}
        */
       optionObjectComparator(value, option) {
-        if (this.isEquivalent(value, option)) {
-          return true
-        } else if (this.index && value === option[this.index]) {
-          return true
-        } else if (this.index && value[this.index] === option[this.index]) {
-          return true
-        } else if ((value[this.label] === option[this.label]) || (value[this.label] === option)) {
-          return true
-        }
-        return false;
+        return this.isEquivalent(value, option);
+
+        // if (this.index && value === option[this.index]) {
+        //   return true
+        // } else if (this.index && value[this.index] === option[this.index]) {
+        //   return true
+        // } else if ((value[this.label] === option[this.label]) || (value[this.label] === option)) {
+        //   return true
+        // }
+        // return false;
       },
 
       /**
